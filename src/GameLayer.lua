9ed373777b5561eec93e8a35e0237be322f17177
Naoyuki Totani <nyk.totani@gmail.com>
2014-11-08 01:07:21 +0900

    player

--- src/GameLayer.lua
+++ src/GameLayer.lua
@@ -7,6 +7,13 @@ end)
 function GameLayer:ctor()
     local map = cc.TMXTiledMap:create("tmx/forest.tmx")
     self:addChild(map)
+    local tile = cc.Sprite:create("img/tile.png")
+    tile:setPosition(100, 200)
+    self:addChild(tile)
+    local player = CCBReaderLoad("PlayerNode.ccbi", cc.CCBProxy:create(), nil)
+    player:setPosition(100, 200)
+    player:setScaleX(-1)
+    self:addChild(player)
 end
 
 function GameLayer.createScene()
