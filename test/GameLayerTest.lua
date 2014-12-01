4cc374d1a868244c7468238a0f87e773da474f3e
Naoyuki Totani <nyk.totani@gmail.com>
2014-12-01 19:45:12 +0900

    初期データ

--- test/GameLayerTest.lua
+++ test/GameLayerTest.lua
@@ -29,28 +29,40 @@ local function main()
 
     director:getOpenGLView():setDesignResolutionSize(360, 640, cc.ResolutionPolicy.FIXED_WIDTH)
 
-    local bs = {}
-    bs["0"] = 10
-    bs["1"] = 11
-    bs["2"] = 12
-    yayoi.Piece:setMaster(0, 1, 1, bs)
-    yayoi.Piece:setMaster(1, 1, 1, bs)
-    yayoi.Piece:setMaster(2, 1, 1, bs)
-    local red  = yayoi.Piece:new(0, bs, 0, 2, 0, true)
-    local blue = yayoi.Piece:new(2, bs, 1, 2, 4, true)
+    local master = cc.FileUtils:getInstance():getStringFromFile("piece.json")
+    master = require("json").decode(master)
+    local jobs = {FIGHTER = 0, MAGE = 1, HEALER = 2}
+    local colosr = { SUN=0, MON=1, TUE=2, WED=3, THU=4, FRI=5, SAT=6 }
+    for k, v in pairs(master) do
+        local bs = {}
+        bs["0"] = v[3]
+        bs["1"] = v[4]
+        bs["2"] = v[5]
+        yayoi.Piece:setMaster(k, v[1], v[2], bs)
+    end
+    local iv = {}
+    iv["0"] = 30
+    iv["1"] = 30
+    iv["2"] = 30
+    local rKnight  = yayoi.Piece:new(0, iv, 0, 1, 0, false)
+    local rKing    = yayoi.Piece:new(1, iv, 0, 2, 0, true)
+    local rWitch   = yayoi.Piece:new(2, iv, 0, 3, 0, false)
+    local bKnight  = yayoi.Piece:new(0, iv, 1, 1, 4, false)
+    local bKing    = yayoi.Piece:new(1, iv, 1, 2, 4, true)
+    local bWitch   = yayoi.Piece:new(2, iv, 1, 3, 4, false)
     local freq = {}
     freq[yayoi.Chip:new( 0, 1)] = 6 -- front
     freq[yayoi.Chip:new( 1, 0)] = 3 -- up
     freq[yayoi.Chip:new(-1, 0)] = 3 -- down
     freq[yayoi.Chip:new( 1, 1)] = 6 -- ufront
     freq[yayoi.Chip:new(-1, 1)] = 6 -- dfront
-    local match = yayoi.Match:new(0, {red, blue}, 5, 5, freq)
+    local match = yayoi.Match:new(0, {rKnight, rKing, rWitch, bKnight, bKing, bWitch}, 5, 5, freq)
     local ctx = {
         on = function(self, event, callback) self.callback = callback end,
         getCorner = function(self) return "red" end,
         act = function(self, playerID, chipID)
             local p = match:getPieces()[playerID + 1]
-            self.callback({match:applyChip(p, chipID), match:applyChip(blue, 0)})
+            self.callback({match:applyChip(p, chipID), match:applyChip(bKnight, 0)})
         end,
         getMatch = function(self) return match end
     }
